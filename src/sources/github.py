bd99560794eb79e14fbec563f0106f81da3790d3
Curtis Larson <quackware@gmail.com>
2013-12-14 11:52:29 -0500

    Fixed up sources a bit

--- src/sources/github.py
+++ src/sources/github.py
@@ -1,6 +1,9 @@
+from sources.source import Source
+from dorks.searchterm import SearchTerm
+
 class GitHub(Source):
 	def __init__(self):
 
 
 	def executeDork(self, dork):
-		
+		for terms in dork.terms:
--- src/sources/pastebin.py
+++ src/sources/pastebin.py
@@ -1,8 +1,12 @@
+from sources.source import Source
+from dorks.searchterm import SearchTerm
+
 class Pastebin(Source):
 	def __init__(self):
 		self.googleUrl = "https://www.google.com/search?q=site:pastebin.com";
 
 	def executeDork(self, dork):
-		for dorkString in dork.dorks:
+		for terms in dork.terms:
+			
 
 		
